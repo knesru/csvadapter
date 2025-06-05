a5634804a83bb51c2ab164a9e5adb9ef33214b90
Fedor Barinov <alloy05@mail.ru>
2025-06-05 09:54:11 +0300

    Update ReportFacade.php

--- ReportFacade.php
+++ ReportFacade.php
@@ -14,4 +14,24 @@ class ReportFacade
         $filePath = $this->csvGenerator->generateFromEntities();
         return $this->csvSender->sendCSV($filePath);
     }
+ public function transformJsonToXml(string $jsonData): string
+    {
+        // Decode JSON data
+        $data = json_decode($jsonData, true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new \InvalidArgumentException('Invalid JSON data provided');
+        }
+
+        // Extract the special field
+        if (!isset($data['super_specail_field'])) {
+            throw new \InvalidArgumentException('Required field "super_specail_field" not found in JSON data');
+        }
+
+        // Create XML structure
+        $xml = new \SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><response></response>');
+        $xml->addChild('external_data', $data['super_specail_field']);
+
+        return $xml->asXML();
+    }
 }
